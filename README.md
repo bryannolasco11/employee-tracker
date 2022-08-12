79de709a00a7c5aa542f35961463a5e519f016c4
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-11 22:33:51 -0500

    installed inquirer and mysql2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # employee-tracker
+
+
+# installation
+inquirer
+npm i inquirer@8.2.4
+
+mysql2
+npm install --save mysql2
---
+++ server.js
@@ -0,0 +1,8 @@
+// 1) install everything
+// 2) use inquiry for:
+//         view all devicePixelRatio, view all roles, 
+//         view all employees, add a department,
+//         add a role, add an empolyee, and 
+//         update employee role
+
+// 3) make functions for all the functions
