0433ca1b9c00080645d7ae070a3b9cec8a845671
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-14 22:14:27 -0500

    employee table displays the manager and the add role is dynamically created for the inquirer.

--- queries.js
+++ queries.js
@@ -6,33 +6,39 @@ const inquirer = require('inquirer');
 const Department = require('./lib/department.js');
 
 // From MYSQL2 docs: MySQL provides execute helper which will prepare and query the statement. 
-const viewDept = ()=> {
+const viewDept = () => {
     //console.log('This is viewDept');
     const sql = `SELECT * FROM department`;
-    connection.query(sql, (err, results)=> {
+    connection.query(sql, (err, results) => {
         if (err) throw err;
-            //console.log(results);
-            // from console.table doc
-            const table = cTable.getTable(results);
-            console.log(table);
-            promptUser();
-        }
-    );  
+        //console.log(results);
+        // from console.table doc
+        const table = cTable.getTable(results);
+        console.log(table);
+        promptUser();
+    }
+    );
 };
 
 
 
 const viewRoles = () => {
-    const sql = `SELECT * FROM department`;
-    connection.query(sql, (err, results)=> {
+    const sql = `SELECT roles.title,
+                        roles.id,
+                        department.dept_name AS department,
+                        roles.salary 
+                FROM roles
+                        LEFT JOIN department ON roles.department_id = department.id
+                `;
+    connection.query(sql, (err, results) => {
         if (err) throw err;
-            //console.log(results);
-            // from console.table doc
-            const table = cTable.getTable(results);
-            console.log(table);
-            promptUser();  
-        }
-    ); 
+        //console.log(results);
+        // from console.table doc
+        const table = cTable.getTable(results);
+        console.log(table);
+        promptUser();
+    }
+    );
 };
 // show employee id, first name, last name, job titles, departments, salaries, and manager
 // from department get department name
@@ -46,55 +52,57 @@ const viewEmployees = () => {
                         employees.first_name,
                         employees.last_name,
                         roles.title,
-                        department.dept_name,
+                        department.dept_name AS department,
                         roles.salary,
-                        CONCAT (first_name, ' ', last_name) AS manager
+                        CONCAT (employees.first_name, ' ', employees.last_name) AS employee,
+                        CONCAT (manager.first_name, ' ', manager.last_name) AS manager
                  FROM employees
                         LEFT JOIN roles ON employees.role_id = roles.id
                         LEFT JOIN department ON roles.department_id = department.id
-                        `; 
-    connection.query(sql, (err, results)=> {
+                        LEFT JOIN employees manager ON employees.manager_id = manager.id
+                        `;
+    connection.query(sql, (err, results) => {
         if (err) throw err;
-            //console.log(results);
-            // from console.table doc
-            const table = cTable.getTable(results);
-            console.log(table);
-            promptUser();
-        }
+        //console.log(results);
+        // from console.table doc
+        const table = cTable.getTable(results);
+        console.log(table);
+        promptUser();
+    }
     )
 };
 
 const addDept = () => {
     inquirer.prompt([
         {
-           type: 'input',
-           name: 'addDept',
-           message: 'What is the name of the department that you wish to add?',
-           validate: inputDept => {
-            if (inputDept) {
-              return true;
-            } else {
-                console.log('Please enter a department name.');
-                return false;
-              }
+            type: 'input',
+            name: 'addDept',
+            message: 'What is the name of the department that you wish to add?',
+            validate: inputDept => {
+                if (inputDept) {
+                    return true;
+                } else {
+                    console.log('Please enter a department name.');
+                    return false;
+                }
             }
-          } 
+        }
     ])
-    .then(answer => {
-        const sql = `INSERT INTO department(dept_name)
+        .then(answer => {
+            const sql = `INSERT INTO department(dept_name)
                     VALUES (?)`;
-                    connection.query(sql, answer.addDept, (err, result) => {
-                        if (err) throw err;
-                        console.log(`You have added ${answer.addDept}!`);
-                        viewDept();
-                    })
-    })
+            connection.query(sql, answer.addDept, (err, result) => {
+                if (err) throw err;
+                console.log(`You have added ${answer.addDept}!`);
+                viewDept();
+            })
+        })
 };
 
 // When choose to add a role
 // 1) prompt enter name, salary, and dept for the role
 // 2) that role is added to the database
-    // INSERT INTO prices (id, price) VALUES (1, 10.55);
+// INSERT INTO prices (id, price) VALUES (1, 10.55);
 // However the department is already there. 
 //So, do we have to push the dept names into an array, 
 // then filter those out to display in a list for the inquiry?
@@ -107,19 +115,19 @@ const addRole = () => {
             message: 'What is the name of the role you wish to add?',
             validate: inputDept => {
                 if (inputDept) {
-                  return true;
+                    return true;
                 } else {
                     console.log('Please enter a department name.');
                     return false;
-                  }
-                } 
+                }
+            }
         },
         {
             type: 'input',
             name: 'salary',
             message: 'What is the salary for the role?',
             validate: inputSalary => {
-                if(inputSalary) {
+                if (inputSalary) {
                     return true;
                 } else {
                     console.log('Please enter a salary.');
@@ -128,59 +136,75 @@ const addRole = () => {
             }
         }
     ])
-    . then(answer => {
-        //use params to put rolename and salary in an array
-        const params = [answer.role, answer.salary];
-        //console.log (answer.role);
-        //console.log (answer.salary);
-        //console.log(params);
+        .then(answer => {
+            //use params to put rolename and salary in an array
 
-        // now I have to grab the department name from the table
-        const deptSql = `Select department.id, department.dept_name FROM department`;
-        connection.query(deptSql, (err, result) =>{
-            if (err) throw err;
-        // Each dept name and title as to go into the inquirer
-        // array.map(function(currentValue, index, arr), thisValue) from w3 schools
-        // const persons = [
-        //     {firstname : "Malcom", lastname: "Reynolds"},
-        //     {firstname : "Kaylee", lastname: "Frye"},
-        //     {firstname : "Jayne", lastname: "Cobb"}
-        //   ];
-          
-        //   persons.map(getFullName);
-          
-        //   function getFullName(item) {
-        //     return [item.firstname,item.lastname].join(" ");
-        //   }
-        
-        //console.log(result); result is in an array
-        const deptNameArray = result;
-        console.log(deptNameArray);
-        //const array1 = [1, 4, 9, 16];
+            //console.log (answer.role);
+            //console.log (answer.salary);
+            //console.log(params);
 
-        // pass a function to map
-        // const map1 = array1.map(x => x * 2);
-        // console.log(map1);
-        // expected output: Array [2, 8, 18, 32]
-        //map((element, index) => { /* … */ })
-        //const deptsForInquiry = deptNameArray.map(({dept_name, id}) => ({department: dept_name, id: id}));
-        
-        
-        console.log(deptsForInquiry);
+            // now I have to grab the department name from the table
+            const deptSql = `Select department.id AS value, department.dept_name AS name FROM department`;
+            connection.query(deptSql, (err, result) => {
+                if (err) throw err;
+                // Each dept name and title as to go into the inquirer
+                // array.map(function(currentValue, index, arr), thisValue) from w3 schools
+                // const persons = [
+                //     {firstname : "Malcom", lastname: "Reynolds"},
+                //     {firstname : "Kaylee", lastname: "Frye"},
+                //     {firstname : "Jayne", lastname: "Cobb"}
+                //   ];
 
-    //     inquirer.prompt([
-    //         {
-    //             type: 'list',
-    //             name: 'department',
-    //             message: 'For which department does the role belong?',
-    //             choices:  //`${dept_name}`
-    //         }
-    //     ])
-         })
-     })
+                //   persons.map(getFullName);
+
+                //   function getFullName(item) {
+                //     return [item.firstname,item.lastname].join(" ");
+                //   }
+
+                //console.log(result); result is in an array
+                const deptNameArray = result;
+                console.log(deptNameArray);
+                //const array1 = [1, 4, 9, 16];
+                //[{ name: "a", value: 1 }]
+                // pass a function to map
+                // const map1 = array1.map(x => x * 2);
+                // console.log(map1);
+                // expected output: Array [2, 8, 18, 32]
+                //map((element, index) => { /* … */ })
+                //const deptsForInquiry = result.map(({dept_name, id}) => ({department: dept_name, id: id}));
+
+
+                //console.log(deptsForInquiry);
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'For which department does the role belong?',
+                        choices: deptNameArray
+                    }
+                ])
+                    .then((answer2) => {
+                        const sql = `INSERT INTO roles (title, department_id, salary)
+                                values
+                                    (?,?,?)`;
+                        const params = [answer.role, answer2.department, answer.salary];
+                        connection.query(sql, params, (err, results) => {
+                            if (err) throw err;
+                            //console.log(results);
+                            // from console.table doc
+                            const table = cTable.getTable(results);
+                            console.log(table);
+                            promptUser();
+                        }
+                        )
+                    })
+            })
+        })
+
 };
 // enter first name, last name, role, and manager 
-const addEmployee = ()=> {
+const addEmployee = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -209,9 +233,9 @@ const addEmployee = ()=> {
             }
         }
     ])
-    .then(answer => {
-        const params = [answer.firstName, answer.lastName]
-    })
+        .then(answer => {
+            const params = [answer.firstName, answer.lastName]
+        })
 
     // need to map the roles from roles table
 };
@@ -220,11 +244,11 @@ function updateRole() {
     console.log('This is updateRole');
 };
 
-module.exports = { 
+module.exports = {
     viewDept,
     viewRoles,
     addDept,
     addRole,
-    viewEmployees, 
+    viewEmployees,
     addEmployee
 }
